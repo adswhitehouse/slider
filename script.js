283888dfc55e12d1feb60a09d339c3985d923bd9
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-22 18:16:08 +0000

    can cycle through slideshow using prev/next images

--- script.js
+++ script.js
@@ -17,8 +17,50 @@ let slides = [
   },
 ];
 
+
+// ----- METHOD 1 -----
+// Declare initial image as 0
+// let initialImage = 0;
+
+// Sets the slideshow background to equal the index initial image index of slides array
+// function setImage() {
+//   slideshow.style.background = `url(${slides[initialImage].src}) no-repeat center center / cover`;
+// }
+
+// Call setImage initially to load first image
+// setImage();
+
+// InitialImage increments and runs setImage function - changing the background to next image and appropriately altering the display buttons
+// nextButton.addEventListener("click", () => {
+//   initialImage++;
+//   setImage();
+//   prevNextButtonHide();
+// });
+
+// InitialImage decrements and runs setImage function - changing the background to previous image and appropriately altering the display buttons
+// prevButton.addEventListener("click", () => {
+//   initialImage--;
+//   setImage();
+//   prevNextButtonHide();
+// });
+
+// If first image is shown - hide prev button. If last image is shown - hide next button.
+// function prevNextButtonHide() {
+//   initialImage >= 1
+//     ? prevButton.classList.remove("hide")
+//     : prevButton.classList.add("hide");
+
+//   initialImage === slides.length - 1
+//     ? nextButton.classList.add("hide")
+//     : nextButton.classList.remove("hide");
+// }
+
+
+// ----- METHOD 2 -----
 // Declare initial image as 0
 let initialImage = 0;
+
+prevButton.classList.remove("hide")
 
 // Sets the slideshow background to equal the index initial image index of slides array
 function setImage() {
@@ -28,27 +70,18 @@ function setImage() {
 // Call setImage initially to load first image
 setImage();
 
-// InitialImage increments and runs setImage function - changing the background to next image and appropriately altering the display buttons
 nextButton.addEventListener("click", () => {
-  initialImage += 1;
-  setImage();
-  prevNextButtonHide();
-});
+  initialImage++
+  if(initialImage === slides.length) {
+    initialImage = 0
+  }
+  setImage()
+})
 
-// InitialImage decrements and runs setImage function - changing the background to previous image and appropriately altering the display buttons
 prevButton.addEventListener("click", () => {
-  initialImage -= 1;
-  setImage();
-  prevNextButtonHide();
-});
-
-// If first image is shown - hide prev button. If last image is shown - hide next button.
-function prevNextButtonHide() {
-  initialImage >= 1
-    ? prevButton.classList.remove("hide")
-    : prevButton.classList.add("hide");
-
-  initialImage === slides.length - 1
-    ? nextButton.classList.add("hide")
-    : nextButton.classList.remove("hide");
-}
+  initialImage--
+  if (initialImage < 0) {
+    initialImage = slides.length - 1
+  }
+  setImage()
+})
