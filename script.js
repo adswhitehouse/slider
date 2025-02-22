c756ddebeee9853f8c7fc4681bd144d5d3d7d5fe
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-22 19:57:33 +0000

    added two methods of functionality

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+// Variables
 let slideshow = document.querySelector(".jsSlideshow");
 let prevButton = document.querySelector(".jsPrevButton");
 let nextButton = document.querySelector(".jsNextButton");
+let slideText = document.querySelector(".jsSlideText");
 
+// Data
 let slides = [
   {
     name: "image-1",
     src: "./images/img-1.jpeg",
+    text: "Slide 1 text",
   },
   {
     name: "image-2",
     src: "./images/img-2.jpeg",
+    text: "Slide 2 text",
   },
   {
     name: "image-3",
     src: "./images/person-1.jpeg",
+    text: "slide 3 text",
   },
 ];
-
 
 // ----- METHOD 1 -----
 // Declare initial image as 0
@@ -55,33 +60,37 @@ let slides = [
 //     : nextButton.classList.remove("hide");
 // }
 
-
 // ----- METHOD 2 -----
 // Declare initial image as 0
 let initialImage = 0;
 
-prevButton.classList.remove("hide")
+// Remove hide class set default for method 1
+prevButton.classList.remove("hide");
 
 // Sets the slideshow background to equal the index initial image index of slides array
 function setImage() {
   slideshow.style.background = `url(${slides[initialImage].src}) no-repeat center center / cover`;
+  // Add text data onto slide
+  slideText.textContent = slides[initialImage].text;
 }
 
 // Call setImage initially to load first image
 setImage();
 
+// InitialImage increments and runs setImage function - changing the background to next image and appropriately altering the display buttons
 nextButton.addEventListener("click", () => {
-  initialImage++
-  if(initialImage === slides.length) {
-    initialImage = 0
+  initialImage++;
+  if (initialImage === slides.length) {
+    initialImage = 0;
   }
-  setImage()
-})
+  setImage();
+});
 
+// InitialImage decrements and runs setImage function - changing the background to previous image and appropriately altering the display buttons
 prevButton.addEventListener("click", () => {
-  initialImage--
+  initialImage--;
   if (initialImage < 0) {
-    initialImage = slides.length - 1
+    initialImage = slides.length - 1;
   }
-  setImage()
-})
+  setImage();
+});
